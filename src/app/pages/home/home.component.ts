17140758156dbe8f0655579b84f2a7f3569ceea0
Jean Gabriel <jean.cantanhede@hitdigital.com.br>
2022-06-23 22:13:37 -0300

    Added modal to create account

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,3 +1,4 @@
+import { AlertService } from './../../services/alert/alert.service';
 import { AccountService } from './../../services/account/account.service';
 import { Account } from './../../shared/models/account';
 import { Component, OnInit } from '@angular/core';
@@ -9,12 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   listAcounts: Account[] = [{ id: '', agency: '', number: '' }];
+  isOpenCreateModal: boolean = false;
+  formAccount: any = { balance: '', number: '' };
 
-  constructor(private accountService: AccountService) {}
+  constructor(
+    private accountService: AccountService,
+    private alertService: AlertService
+  ) {}
 
   ngOnInit(): void {
+    this.loadListAccounts();
+  }
+
+  showCreateModal(): void {
+    this.isOpenCreateModal = true;
+  }
+  closeCreateModal(): void {
+    this.isOpenCreateModal = false;
+  }
+
+  loadListAccounts(): void {
     this.accountService.list().subscribe((accounts: Account[]) => {
       this.listAcounts = accounts;
+    });
+  }
+
+  onClickSubmit() {
+    this.accountService.create(this.formAccount).subscribe({
+      next: (res) => {
+        this.loadListAccounts();
+        this.isOpenCreateModal = false;
+        this.alertService.success('Account created successfully', 'Success');
+      },
+      error: (res) => {
+        this.alertService.error(
+          'Error trying to create account. Check if you already have a registered account',
+          'Error'
+        );
+      },
     });
   }
 }
