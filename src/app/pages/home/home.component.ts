acaa45275aec284c8909120950fc171af70e28b1
Jean Gabriel <jean.cantanhede@hitdigital.com.br>
2022-06-23 21:05:34 -0300

    Updated home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,20 @@
+import { AccountService } from './../../services/account/account.service';
+import { Account } from './../../shared/models/account';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  listAcounts: Account[] = [{ id: '', agency: '', number: '' }];
 
-  constructor() { }
+  constructor(private accountService: AccountService) {}
 
   ngOnInit(): void {
+    this.accountService.list().subscribe((accounts: Account[]) => {
+      this.listAcounts = accounts;
+    });
   }
-
 }
